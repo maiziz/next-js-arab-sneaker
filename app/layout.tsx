66d25a4e867e312399031ea4d67c1e669654a0ef
maiziz <maiziz.ahmed@gmail.com>
2024-12-01 10:44:31 +0100

    init arab nextjs sneaker

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html lang="ar" dir="rtl">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
